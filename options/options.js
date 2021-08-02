9906b46a3172f0c133814a88fad986c6cae2cdb7
Crowdbotics <team@crowdbotics.com>
2021-08-02 21:59:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tight_hall_29349Identifier",
+  url: "https://tight_hall_29349Identifier.botics.co",
+  api: "https://tight_hall_29349Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
